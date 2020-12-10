7266fd90ad33590e11384209bbf96d088855bba6
Andreas Appelros <andreas.appelros@gmail.com>
2020-12-10 15:10:47 +0100

    Day 10

--- AoC2020/AoC2020/Day10.cs
+++ AoC2020/AoC2020/Day10.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AoC2020
