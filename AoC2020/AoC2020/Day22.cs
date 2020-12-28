22bee26129f9cfe2335905ce6c88953030d68591
Andreas Appelros <andreas.appelros@gmail.com>
2020-12-28 19:04:34 +0100

    Day 22 part 2

--- AoC2020/AoC2020/Day22.cs
+++ AoC2020/AoC2020/Day22.cs
@@ -59,6 +59,95 @@ namespace AoC2020
             TestContext.WriteLine($"{sum}");
         }
 
+        [TestMethod]
+        public void Part2()
+        {
+            // Iterate over lines
+            var stringReader = new StringReader(DayInput);
+            string line;
+            var player1 = new Queue<int>();
+            var player2 = new Queue<int>();
+            var currentDeck = player1;
+            while ((line = stringReader.ReadLine()) != null)
+            {
+                if (string.IsNullOrEmpty(line))
+                    currentDeck = player2;
+                if (int.TryParse(line, out var card) == false)
+                    continue;
+                currentDeck.Enqueue(card);
+            }
+
+
+            PlayGame(player1, player2);
+
+
+            if (player1.Count > 0)
+                currentDeck = player1;
+            else
+                currentDeck = player2;
+
+            var sum = currentDeck.Select((c, i) => (c, currentDeck.Count - i)).Sum(t => t.c * t.Item2);
+            TestContext.WriteLine($"{sum}");
+        }
+
+        private static int PlayGame(Queue<int> player1, Queue<int> player2)
+        {
+            var previousRounds = new HashSet<string>();
+            while (player1.Count != 0 && player2.Count != 0)
+            {
+                if (previousRounds.Add(ToStringRepresentation(player1, player2)) == false)
+                {
+                    return 1;
+                }
+                var card1 = player1.Dequeue();
+                var card2 = player2.Dequeue();
+                if (player1.Count >= card1 && player2.Count >= card2)
+                {
+                    var winner = PlayGame(new Queue<int>(player1.Take(card1)), new Queue<int>(player2.Take(card2)));
+                    if (winner == 1)
+                    {
+                        player1.Enqueue(card1);
+                        player1.Enqueue(card2);
+                    }
+                    else
+                    {
+                        player2.Enqueue(card2);
+                        player2.Enqueue(card1);
+                    }
+                }
+                else if (card1 > card2)
+                {
+                    player1.Enqueue(card1);
+                    player1.Enqueue(card2);
+                }
+                else
+                {
+                    player2.Enqueue(card2);
+                    player2.Enqueue(card1);
+                }
+            }
+
+            return player1.Count > 0 ? 1 : 2;
+        }
+
+        private static string ToStringRepresentation(IEnumerable<int> player1, IEnumerable<int> player2)
+        {
+            var stringBuilder = new StringBuilder("Player 1:");
+            stringBuilder.AppendLine();
+            foreach (var card in player1)
+            {
+                stringBuilder.AppendLine($"{card}");
+            }
+            stringBuilder.AppendLine();
+            stringBuilder.AppendLine("Player 2:");
+            foreach (var card in player2)
+            {
+                stringBuilder.AppendLine($"{card}");
+            }
+
+            return stringBuilder.ToString();
+        }
+
         private void DebugOutput(IEnumerable<int> deck)
         {
             foreach (var card in deck)
@@ -66,6 +155,15 @@ namespace AoC2020
                 TestContext.WriteLine(card.ToString());
             }
         }
+
+        private string TestInput2 = @"Player 1:
+43
+19
+
+Player 2:
+2
+29
+14";
 
         private string TestInput = @"Player 1:
 9
